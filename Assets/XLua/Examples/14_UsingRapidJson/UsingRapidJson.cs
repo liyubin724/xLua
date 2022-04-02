6dfd9d7f5b71381848a7b51985b7810213e39d11
li.yubin <liyubin724@163.com>
2022-04-02 11:14:51 +0800

    feature[rapidjson]:添加RapidJson的集成测试与使用方法

---
+++ Assets/XLua/Examples/14_UsingRapidJson/UsingRapidJson.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+public class UsingRapidJson : MonoBehaviour
+{
+    public TextAsset luaScript;
+
+    void Start()
+    {
+        LuaEnv luaEnv = new LuaEnv();
+        luaEnv.DoString(luaScript.text, "LuaTestScript");
+        luaEnv.Dispose();
+    }
+
+    
+}
