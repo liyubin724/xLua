bf6c15cb49db251c975ba98fdce99a79f1892576
li.yubin <liyubin724@163.com>
2022-04-04 12:47:36 +0800

    feature[ui]:添加支持Lua的UI控件及相关的事件侦听

---
+++ Assets/Scripts/CSScripts/DotEditor/Editor/UI/LuaButtonEditor.cs
@@ -0,0 +1,38 @@
+﻿using DotEngine.UI;
+using UnityEditor;
+using UnityEditor.UI;
+
+namespace DotEditor.UI
+{
+    [CustomEditor(typeof(LuaButton), true)]
+    public class LuaButtonEditor : ButtonEditor
+    {
+        SerializedProperty binderBehaviourProperty;
+        SerializedProperty clickedFuncNameProperty;
+        SerializedProperty paramValuesProperty;
+
+        ReorderableListProperty paramValuesRLProperty;
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            binderBehaviourProperty = serializedObject.FindProperty("binderBehaviour");
+            clickedFuncNameProperty = serializedObject.FindProperty("clickedFuncName");
+            paramValuesProperty = serializedObject.FindProperty("paramValues");
+
+            paramValuesRLProperty = new ReorderableListProperty(paramValuesProperty);
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            {
+                EditorGUILayout.PropertyField(binderBehaviourProperty);
+                EditorGUILayout.PropertyField(clickedFuncNameProperty);
+
+                paramValuesRLProperty.OnGUILayout();
+            }
+            serializedObject.ApplyModifiedProperties();
+            base.OnInspectorGUI();
+        }
+    }
+}
