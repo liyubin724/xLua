e08c8d63c7cda7ea81444ea450013048e0783de2
li.yubin <liyubin724@163.com>
2022-04-10 12:24:11 +0800

    feature[ui]:添加部分UI控件

---
+++ Assets/Scripts/CSScripts/DotEditor/Editor/UI/UIRawImageEditor.cs
@@ -0,0 +1,34 @@
+﻿using DotEngine.UI;
+using UnityEditor;
+using UnityEditor.UI;
+
+namespace DotEditor.UI
+{
+    [CustomEditor(typeof(UIRawImage))]
+    public class UIRawImageEditor : RawImageEditor
+    {
+        SerializedProperty imageAddressProperty;
+        SerializedProperty isSetNativeSizeProperty;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            imageAddressProperty = serializedObject.FindProperty("m_ImageAddress");
+            isSetNativeSizeProperty = serializedObject.FindProperty("m_IsSetNativeSize");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            EditorGUILayout.Space();
+
+            serializedObject.Update();
+            {
+                EditorGUILayout.PropertyField(imageAddressProperty);
+                EditorGUILayout.PropertyField(isSetNativeSizeProperty);
+            }
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
